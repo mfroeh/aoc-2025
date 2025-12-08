b4395aee1f06a804e1905967c40aef8f04d1aeaa
mfroeh <mfroeh0@pm.me>
2025-12-08 18:12:15 +0100

    day 8 part1 :star:

---
+++ 8/a.hs
@@ -0,0 +1,37 @@
+import Data.List
+import Data.List.Split
+import Data.Maybe
+
+type Coordinate = (Int, Int, Int)
+
+dist :: Coordinate -> Coordinate -> Double
+dist (a1, a2, a3) (b1, b2, b3) = sqrt $ fromIntegral ((a1 - b1) ^ 2 + (a2 - b2) ^ 2 + (a3 - b3) ^ 2)
+
+parseCoordinate :: String -> Coordinate
+parseCoordinate str =
+  let s = read <$> splitOn "," str :: [Int]
+   in (s !! 0, s !! 1, s !! 2)
+
+findContainingSet :: Coordinate -> [[Coordinate]] -> [Coordinate]
+findContainingSet c (x : xs) = if isJust $ find (== c) x then x else findContainingSet c xs
+
+mergeSets :: (Coordinate, Coordinate) -> [[Coordinate]] -> [[Coordinate]]
+mergeSets (p, q) sets =
+  let pSet = findContainingSet p sets
+      qSet = findContainingSet q sets
+   in if pSet == qSet
+        then sets
+        else [set | set <- sets, set /= pSet && set /= qSet] ++ [pSet ++ qSet]
+
+mergeSets' :: [(Coordinate, Coordinate)] -> [[Coordinate]] -> [[Coordinate]]
+mergeSets' [] sets = sets
+mergeSets' (x:xs) sets = mergeSets' xs $ mergeSets x sets
+
+main :: IO ()
+main = do
+  coordinates <- (readFile "8/input.txt") >>= (pure . (fmap parseCoordinate) . lines)
+  let pairs = [(coordinates !! i, coordinates !! j) | i <- [0 .. length coordinates - 1], j <- [(i + 1) .. length coordinates - 1]]
+  let ordered = sortBy (\(p1, q1) (p2, q2) -> (dist p1 q1) `compare` (dist p2 q2)) pairs
+  let sets = [[c] | c <- coordinates]
+  let res = foldl (*) 1 $ take 3 $ sortBy (flip compare) $ length <$> mergeSets' (take 1000 ordered) sets
+  print res
